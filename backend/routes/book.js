b4261c005f563617a2f119c6d8b4bfd8b99c2d08
Ankush Varute <ankush.varute705@gmail.com>
2024-11-18 19:44:54 +0530

    add book--admin

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -31,26 +31,26 @@ router.post("/add-book", authenticationToken, async (req, res) => {
 
 });
 
-//updateBook
-router.put("/update-book", authenticationToken, async (req, res)=>{
-    try {
-        const {bookid} = req.headers;
-        await Book.findByIdAndUpdate(bookid, {
+// //updateBook
+// router.put("/update-book", authenticationToken, async (req, res)=>{
+//     try {
+//         const {bookid} = req.headers;
+//         await Book.findByIdAndUpdate(bookid, {
         
-            url: req.body.url,
-            title: req.body.title,
-            author: req.body.author,
-            price: req.body.price,
-            decs: req.body.decs,
-            language: req.body.language
-        });
-        return res.status(200)
-        .json({message: "Book update  successfully"})
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Internal server error" });
-    }
+//             url: req.body.url,
+//             title: req.body.title,
+//             author: req.body.author,
+//             price: req.body.price,
+//             decs: req.body.decs,
+//             language: req.body.language
+//         });
+//         return res.status(200)
+//         .json({message: "Book update  successfully"})
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).json({ message: "Internal server error" });
+//     }
 
-})
+// })
 
 module.exports = router;
