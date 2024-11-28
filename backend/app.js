2e8d70ee81e0ca508b54206caf45c45f3b185392
Ankush Varute <ankush.varute705@gmail.com>
2024-11-28 11:46:54 +0530

    add routes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const  connectDB  = require ("./connection/db")
+const User = require("./routes/user.route");
+const Books = require("./routes/book")
+const Favourite = require("./routes/favourite")
 
 // app.get('/' , (req, res) =>{
 //     res.send('Hello from backend')
@@ -11,7 +14,9 @@ connectDB();
 
 app.use(express.json());
 //routes
-app.use("/api/v1/sign-up",user); 
+app.use("/api/v1/",User); 
+app.use("/api/v1/",Books); 
+app.use("/api/v1/",Favourite); 
 
 // creating port
 app.listen(process.env.PORT, () => {
