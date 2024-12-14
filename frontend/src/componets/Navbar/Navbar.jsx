90c95c538fec22c72b3194a8dab6d36fb2ff375e
Ankush Varute <ankush.varute705@gmail.com>
2024-12-14 13:30:37 +0530

    Routes

--- frontend/src/componets/Navbar/Navbar.jsx
+++ frontend/src/componets/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
+import { Link, useViewTransitionState } from 'react-router-dom';
 import { FaGripLines } from "react-icons/fa";
 
 const Navbar = () => {
@@ -26,9 +26,10 @@ const Navbar = () => {
       link: "/profile",
     },
   ];
+  const [MobileNav, setMobileNav] = useState("hidden")
   return (
     <>
-    <nav className=' relative flex bg-zinc-800 text-white px-8 py-4 items-center justify-between'>
+    <nav className='z-50 relative flex bg-zinc-800 text-white px-8 py-4 items-center justify-between'>
       <div className='flex items-center'>
         <img
           className='h-10 me-4'
@@ -52,12 +53,27 @@ const Navbar = () => {
            className='px-2 py-1 bg-blue-500  rounded  hover:bg-white hover:text-zinc-800 transition-all duration-300 '>SignUp</Link>
           
         </div>
-        <button className='text-2xl text-white hover:text-zinc-400'>
+        <button className='text-2xl text-white hover:text-zinc-400' onClick={()=>(MobileNav ==="hidden" ? setMobileNav("block") :setMobileNav ("hidden"))}>
         <FaGripLines />
         </button>
       </div>
     </nav>
-    <div className='bg-zinc-800 h-screen absolute top-0 left-0'></div>
+    <div className={`${MobileNav} bg-zinc-800 h-screen absolute top-0 left-0 w-full z-40 flex flex-col items-center  justify-center`}>
+    {links.map((items, i) => (
+            <Link to={items.link} 
+            className={`${MobileNav} text-white text-4xl font font-semibold mb-8 hover:text-blue-500   transition-all duration-300`} key={i}
+            onClick={()=>(MobileNav ==="hidden" ? setMobileNav("block") :setMobileNav ("hidden"))}
+            >{items.title}{" "}
+            </Link>
+          ))}
+          
+          <Link to='/LogIn' 
+          className={`${MobileNav} px-8 mb-8 py-2 text-3xl font-semibold border  border-blue-500 rounded text-white   hover:bg-white hover:text-zinc-800 transition-all duration-300`}>LogIn</Link>  
+          <Link to="/SignUp" 
+           className={`${MobileNav} px-8 mb-5 py-2  text-3xl font-semibold bg-blue-500  rounded  hover:bg-white hover:text-zinc-800 transition-all duration-300`}>SignUp</Link>
+          
+       
+    </div>
     </>
   )
 }
