543ebb86332afd8074515cc5f63748f6a96e0447
Ankush Varute <ankush.varute705@gmail.com>
2024-12-14 10:23:16 +0530

    footer

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -5,7 +5,7 @@ const { authenticateToken } = require("./userAuth");
 
 //add to cart 
 
-router.put("/add-to-cart", authenticateToken , async (req, res ) => {
+router.put("/add-to-cart", authenticateToken , async (req, res) => {
  try {
     const { bookid, id } = req.headers;
     const userData = await User.findById(id);
