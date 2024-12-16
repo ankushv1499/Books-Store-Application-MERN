f548b377b6c1b3a440239962831d754449c2b373
Ankush Varute <ankush.varute705@gmail.com>
2024-12-16 10:50:39 +0530

    import Recently added file

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../componets/Navbar/Navbar'
 import Footer from '../componets/Footer/Footer'
 import Hero from '../componets/Home/Hero'
+import RecentlyAdded from '../componets/Home/RecentlyAdded'
 
 
 const Home = () => {
   return (
     <div className='bg-zinc-900 text-white px-10 py-8 '>
     <Hero></Hero>
+    <RecentlyAdded></RecentlyAdded>
     </div>
   )
 }
